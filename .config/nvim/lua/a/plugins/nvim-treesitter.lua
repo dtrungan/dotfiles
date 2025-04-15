19cfce29be53a5693cb987d3a5f2492d11d09620
dtrungan <dtrungan.dev@gmail.com>
2025-04-15 16:02:27 +0700

    Add markdown treesitter for neovim

--- .config/nvim/lua/a/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/a/plugins/nvim-treesitter.lua
@@ -5,7 +5,7 @@ return {
         local configs = require("nvim-treesitter.configs")
 
         configs.setup({
-            ensure_installed = { "lua", "vim" },
+            ensure_installed = { "lua", "vim", "markdown", "markdown_inline" },
             sync_install = false,
             highlight = { enable = true },
             indent = { enable = true },
