4d28b21979c29cc1e8837d784e7b18015346e529
dtrungan <dtrungan.dev@gmail.com>
2025-09-02 09:35:16 +0700

    Add LSP binds

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -32,6 +32,40 @@ vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
 
+-- LSP Navigation
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
+vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
+vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, { desc = "Go to type definition" })
+vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references" })
+
+-- LSP Documentation
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show hover documentation" })
+vim.keymap.set("n", "<leader>k", vim.lsp.buf.signature_help, { desc = "Show signature help" })
+
+-- LSP Code Actions
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("v", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
+
+-- LSP Formatting
+vim.keymap.set("n", "<leader>lf", function()
+  vim.lsp.buf.format({ async = true })
+end, { desc = "Format document" })
+
+-- LSP Diagnostics
+vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show line diagnostics" })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Previous diagnostic" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Next diagnostic" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Add diagnostics to location list" })
+
+-- LSP Workspace
+vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, { desc = "Add workspace folder" })
+vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, { desc = "Remove workspace folder" })
+vim.keymap.set("n", "<leader>wl", function()
+  print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, { desc = "List workspace folders" })
+
 -- nvim-ale config
 vim.cmd [[
     let g:ale_linters = {
--- .config/nvim/lua/a/plugins/which-key.lua
+++ .config/nvim/lua/a/plugins/which-key.lua
@@ -5,9 +5,19 @@ return {
         vim.o.timeout = true
         vim.o.timeoutlen = 300
     end,
-    opts = {
-        -- your configuration comes here
-        -- or leave it empty to use the default settings
-        -- refer to the configuration section below
-    },
+    config = function()
+        local wk = require("which-key")
+
+        -- wk.setup({
+        --     preset = "modern",
+        -- })
+
+        wk.add({
+            { "<leader>f", group = "Telescope" },
+            { "<leader>c", group = "Code action" },
+            { "<leader>l", group = "Format document" },
+            { "<leader>r", group = "Rename symbol" },
+            { "<leader>w", group = "Workspace" },
+        })
+    end,
 }
